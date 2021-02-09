0ebb2fe223672ac24273d992e07fb1244ff59d8d
CRG <69594101+CoolRadGamer@users.noreply.github.com>
2021-02-09 21:58:53 +0000

    test

--- js/mod.js
+++ js/mod.js
@@ -42,7 +42,7 @@ function getPointGen() {
 		return new Decimal(0)
 
 	let gain = new Decimal(1)
-	if (hasUpgrade('cn', 11)) gain.times("CRG necks")
+	if (hasUpgrade('cn', 11)) gain = gain.times(player.cn.points)
 	return gain
 }
 
