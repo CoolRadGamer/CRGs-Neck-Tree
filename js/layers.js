66968e0bfbc7252e09dde06bf931ba2a27020124
CRG <69594101+CoolRadGamer@users.noreply.github.com>
2021-02-09 21:58:09 +0000

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -15,7 +15,7 @@ addLayer("cn", {
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
-	    if (hasUpgrade('cn', 11)) mult.times("CRG necks")
+	    if (hasUpgrade('cn', 11)) mult.times(player.cn.points)
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
